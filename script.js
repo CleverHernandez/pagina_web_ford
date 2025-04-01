f5454582e25552dbf54a4e84d05f5ac8aa5a2844
Clever Hernandez <220586@mail.unacar.mx>
2025-04-01 17:28:37 -0600

    Modifique y añadi archivos

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth * 0.8, 500);
+renderer.setSize(window.innerWidth * 0.5, 390);
 document.getElementById('viewer').appendChild(renderer.domElement);
 
-// Luz
 const light = new THREE.DirectionalLight(0xffffff, 2);
 light.position.set(2, 2, 2);
 scene.add(light);
@@ -12,29 +11,30 @@ scene.add(light);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
-// Crear un material para el video
 const video = document.createElement('video');
-video.src = 'video.mp4'; // Asegúrate de que el video esté en el directorio correcto
+video.src = './video/fordGT40.mp4'; 
+video.loop = true;
+video.muted = true;  
 video.load();
-video.play();
-video.loop = true;  // Reproducir en bucle
-video.muted = false;  // Puedes ponerlo en mute si lo prefieres
+video.playbackRate = 0.5;  
+
+video.addEventListener('loadeddata', () => {
+    console.log("Video cargado correctamente");
+    video.play();
+});
 
 const texture = new THREE.VideoTexture(video);
+texture.needsUpdate = true; 
 const material = new THREE.MeshBasicMaterial({ map: texture });
 
-// Crear un plano para aplicar la textura del video
-const geometry = new THREE.PlaneGeometry(16, 9); // Tamaño del plano
+const geometry = new THREE.PlaneGeometry(42, 22);
 const plane = new THREE.Mesh(geometry, material);
 scene.add(plane);
 
-// Ajustar la posición del plano
-plane.position.z = -5;  // Mover el plano hacia atrás
+plane.position.z = -5;
 
-// Cámara
-camera.position.z = 10;
+camera.position.set(0, 0, 10);
 
-// Animación
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
